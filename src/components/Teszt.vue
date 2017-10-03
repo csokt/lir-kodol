17d8b2ed297da73ac38bd34433fffd46a2465702
Csók Tibor <csokt0@gmail.com>
2017-10-03 14:55:47 +0200

    Initial commit

--- src/components/Teszt.vue
+++ src/components/Teszt.vue
@@ -1,108 +1,22 @@
 <template>
-  <div class="qrcode-reader">
-    <input type="number" @keyup.enter="submit" v-model="input" placeholder="Kézi adatbevitel"/>
-    <button v-if="cameraCount > 1" @click="activeCamera += 1" type="button">Váltás az előlapi és hátlapi kamera között</button>
-    <br>
-    <video class="camera" ref="preview"></video>
-    <br>
-    {{ message }}
+  <div>
+    <h1>{{ msg }}</h1>
+    <qrcode-reader></qrcode-reader>
   </div>
-
 </template>
 
 <script>
-import Instascan from 'instascan-ngfar'
-// import Instascan from 'instascan'
+import QrcodeReader from './QrcodeReader.vue'
 
 export default {
-  props: {
-    message: {
-      type: String,
-      default: ''
-    },
-
-    // Whether to horizontally mirror the video preview.
-    mirror: {
-      type: Boolean,
-      default: true
-    },
-
-    // Whether to actively scan when the tab is not active.
-    backgroundScan: {
-      type: Boolean,
-      default: true
-    },
-
-    // The period, in milliseconds, before the same QR code will be recognized in succession.
-    refractoryPeriod: {
-      type: Number,
-      default: 5000
-    },
-
-    // Only applies to continuous mode. The period, in rendered frames, between scans.
-    scanPeriod: {
-      type: Number,
-      default: 10
-    },
-
-    // Default camera index, begins with 0.
-    defaultCamera: {
-      type: Number,
-      default: 0
-    }
-
-  },
-
+  name: 'hello',
   data () {
     return {
-      input: '',
-      cameras: [],
-      cameraCount: 0,
-      activeCamera: -1
+      msg: 'Teszt'
     }
   },
-
-  watch: {
-    activeCamera (newValue) {
-      if (this.cameraCount === 0) {
-        console.error('No cameras found.')
-      } else if (newValue < this.cameraCount) {
-        let camera = this.cameras[newValue]
-        this.scanner.start(camera)
-      } else {
-        this.activeCamera = 0
-      }
-    }
-  },
-
-  mounted () {
-    let self = this
-    let opts = { video: self.$refs.preview, mirror: self.mirror, backgroundScan: self.backgroundScan, refractoryPeriod: self.refractoryPeriod, scanPeriod: self.scanPeriod }
-    self.scanner = new Instascan.Scanner(opts)
-
-    self.scanner.addListener('scan', function (content) {
-//      self.input = content
-      self.$emit('capture', content)
-    })
-
-    Instascan.Camera.getCameras().then(function (cameras) {
-      self.cameras = cameras
-      self.cameraCount = cameras.length
-      self.activeCamera = self.defaultCamera
-    }).catch(function (e) {
-      console.error(e)
-    })
-  },
-
-  beforeDestroy: function () {
-    this.scanner.stop()
-  },
-
-  methods: {
-    submit () {
-      this.$emit('capture', this.input)
-    }
-
+  components: {
+    'qrcode-reader': QrcodeReader
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 import Hello from '@/components/Hello'
 import Teszt from '@/components/Teszt'
 
 Vue.use(Router)
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 
 export default new Router({
   routes: [
